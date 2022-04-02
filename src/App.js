f8a7abab1c6bf3697d333689ed4ad4086bb3c618
Justin Proulx <jawproulx2001@icloud.com>
2022-04-02 19:54:14 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+import DentistUI from './UI/DentistUI';
+import PatientUI from './UI/PatientUI';
+import ReceptionistUI from './UI/ReceptionistUI';
+import TriageUI from './UI/TriageUI';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<TriageUI />} />
+          <Route path='/patient' element={<PatientUI />}></Route>
+          <Route path='/dentist' element={<DentistUI />}></Route>
+          <Route path='/receptionist' element={<ReceptionistUI />}></Route>
+        </Routes>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/UI/TriageUI.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class TriageUI extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Login page with a pun!</h1>
+				<p>Nothing here yet</p>
+			</div>
+		)
+	}
+}
