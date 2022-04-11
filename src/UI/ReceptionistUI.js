2e46f77e2db2c555928dbb537d300d40d986029f
Justin Proulx <jawproulx2001@icloud.com>
2022-04-10 22:46:10 -0400

    add new patient ui

--- src/UI/ReceptionistUI.js
+++ src/UI/ReceptionistUI.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import NewPatientUI from './NewPatientUI';
 
 export default class ReceptionistUI extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Welcome, [insert receptionist name here]</h1>
-				<p>Receptionist UI goes here</p>
+				<button>New Patient</button>
+				<NewPatientUI></NewPatientUI>
 			</div>
 		)
 	}
