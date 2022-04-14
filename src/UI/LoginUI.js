3f826afb1d237a6acc3466dd2433ffb2813c05cd
Arunasva (Ash) <43392585+ArunasvaArunasva@users.noreply.github.com>
2022-04-14 19:36:25 -0400

    removing junk form the junk

--- src/UI/LoginUI.js
+++ src/UI/LoginUI.js
@@ -4,28 +4,21 @@ import { useHistory } from 'react-router-dom';
 export default function LoginUI(){
     const [userId,setUserId] = useState("");
     const[password,setPassword] = useState("");
-    const history = useHistory();
+    // const history = useHistory();
 
-    async function login(){
-       console.log(userId +" "+ password)
-       let item = {userId,password};
-       let deets = await fetch("",{
-           method:'POST',
-           headers:{
-               //TODO
-           },
-           body:JSON.stringify(item),
-           });
-       deets = await deets.json();
-       localStorage.setItem(JSON.stringify(deets))
-        if(localStorage.getItem('SSN' in patient) && password in patient){
-            history.push("/patient")
-        }else if(localStorage.getItem('SSN') in dentist  && password in dentist){
-            history.push("/dentist")
-        }else if(localStorage.getItem('SSN')in receptionist && password in receptionist){
-              history.push("/receptionist")
-        }
-   }
+   //  async function login(){
+   //     console.log(userId +" "+ password)
+   //     let item = {userId,password};
+   //     let deets = await fetch("",{
+   //         method:'POST',
+   //         headers:{
+   //             //TODO
+   //         },
+   //         body:JSON.stringify(item),
+   //         });
+   //     deets = await deets.json();
+   //     localStorage.setItem(JSON.stringify(deets));
+   // }
     return(
         <div>
             <input type="text" placeholder="userID"  onChange={(e) => setUserId(e.target.value)}
@@ -34,7 +27,7 @@ export default function LoginUI(){
             <input type="text" placeholder="password"   onChange={(e) => setUserId(e.target.value)}
                 className="form-control"/>
             <br></br>
-            <button onClick={login}>LOGIN</button>
+            <button >LOGIN</button>
         </div>
     )
 }
