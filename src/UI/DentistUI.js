37cb6e818557f0a586cb33557f04b88708b16919
Arunasva (Ash) <43392585+ArunasvaArunasva@users.noreply.github.com>
2022-04-15 18:38:31 -0400

    Employee Page Showing Employee Name

--- src/UI/DentistUI.js
+++ src/UI/DentistUI.js
@@ -2,12 +2,10 @@ import React, {Component, useEffect, useState} from 'react';
 import {useLocation} from "react-router-dom";
 
 export default function DentistUI() {
-	// let location = useLocation();
-	// const [empName,setUserName] = useState(location.state);
-
+	let location = useLocation();
 		return (
 			<div>
-				<h1>Welcome {empName}</h1>
+				<h1><h1>Welcome {location.state.username}</h1></h1>
 				<h2>Assigned Jobs</h2>
 				<button>Show Appointments</button>
 			</div>
