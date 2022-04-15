f0066b474565a0d00b67004e8298f4ea12bee3a9
Daniel Shwan <dannyshwan@gmail.com>
2022-04-15 15:38:59 -0400

    fixed

--- src/UI/LoginUI.js
+++ src/UI/LoginUI.js
@@ -1,45 +1,43 @@
-import {useState,  useEffect} from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import { useNavigate } from 'react-router-dom';
-export default function LoginUI(){
-    const [userName,setUserId] = useState("");
-    const[password,setPassword] = useState("");
-    const nav = useNavigate();
+export default function LoginUI() {
+	const [userName, setUserId] = useState('');
+	const [password, setPassword] = useState('');
+	const nav = useNavigate();
 
-    useEffect(()=>{
-        if(localStorage.getItem('user-info')){
-            nav.navigate("/patient")
-        }
-    },[])
-    
-    async function loginPatient(){
-       let item ={username:userName, password};
-       let result = await fetch("http://localhost:8000/users/login",{
-           method:'POST',
-           headers:{
-               "Content-Type":"application/json",
-               "Accept":"application/json"
-           },
-           body: JSON.stringify(item)
-       });
-        alert(result);
-        alert("breakpoint")
-       result = await result.json();
-       localStorage.setItem("user-info",JSON.stringify(result));
-       nav.navigate('/patient')
-   }
-    return(
-        <div>
-            <h1>LOGIN PAGE</h1>
-            <input type="text" placeholder="username"
-                   onChange={(e) => setUserId(e.target.value)}
-                   className="form-control"/>
-            <br></br>
-            <input type="text" placeholder="password"
-                   onChange={(e) => setPassword(e.target.value)}
-                   className="form-control"/>
-            <br></br>
-            <button onClick={loginPatient} >LOGIN</button>
-        </div>
-    )
+	useEffect(() => {
+		if (localStorage.getItem('user-info')) {
+			nav('/patient');
+		}
+	}, []);
+
+	async function loginPatient() {
+		let item = { username: userName, password };
+		let result = (await axios.post('http://localhost:8000/users/login', item))
+			.data;
+		localStorage.setItem('user-info', result.user_type);
+		nav('/patient');
+	}
+	return (
+		<div>
+			<h1>LOGIN PAGE</h1>
+			<input
+				type="text"
+				placeholder="username"
+				onChange={(e) => setUserId(e.target.value)}
+				className="form-control"
+			/>
+			<br></br>
+			<input
+				type="text"
+				placeholder="password"
+				onChange={(e) => setPassword(e.target.value)}
+				className="form-control"
+			/>
+			<br></br>
+			<button onClick={loginPatient}>LOGIN</button>
+		</div>
+	);
 }
