72e01792ae130b19e970b0dbcec6f466fe48b0d5
Justin Proulx <jawproulx2001@icloud.com>
2022-04-15 20:25:43 -0400

    fixed

--- src/UI/PatientUI.js
+++ src/UI/PatientUI.js
@@ -1,47 +1,76 @@
-import React, { Component } from 'react';
-import {useLocation} from "react-router-dom";
+import React, { Component, useEffect, useState } from 'react';
+import { useLocation } from "react-router-dom";
 
-export default function PatientUI(){
-		let location = useLocation();
-		let currentUser = location.state.username;
-		let currentPassword = location.state.password;
+export default function PatientUI(props) {
+	let location = useLocation();
+	let currentUser = location.state.username;
+	let currentPassword = location.state.password;
 
+	const [apps, setApps] = useState([]);
+	const [loaded, setLoaded] = useState(false);
 
-		async function showApp(){
-			//get user id from login
-			let item ={username:currentUser, password:currentPassword};
-			let result = await fetch("http://localhost:8000/users/login",{
-				method:'POST',
-				headers:{
-					"Content-Type":"application/json",
-					"Accept":"application/json"
-				},
-				body: JSON.stringify(item)
+	useEffect(() => {
+		if (!loaded) {
+			getApp().then(result => {
+				setLoaded(true);
 			});
-			result = await result.json();
-			localStorage.setItem("user-info",JSON.stringify(result));
-			let patientID = result.id;
+		}
+	})
 
-			//use patient id to obtain appointments
-			let apps = await fetch(`http://localhost:8000/appointments/patient/${patientID}`,{
-				method:'GET',
-				headers:{
-					"Content-Type":"application/json",
-					"Accept":"application/json"
-				},
-			});
-			apps = await apps.json();
-			let status = apps.status;
-			localStorage.setItem("user-info",JSON.stringify(apps));
+	async function getApp() {
+		//get user id from login
+		let item = { username: currentUser, password: currentPassword };
+		let result = await fetch("http://localhost:8000/users/login", {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify(item)
+		});
+		result = await result.json();
+		localStorage.setItem("user-info", JSON.stringify(result));
+		let patientID = result.id;
+
+		//use patient id to obtain appointments
+		let apps = await fetch(`http://localhost:8000/appointments/patient/${patientID}`, {
+			method: 'GET',
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+		});
+		apps = await apps.json();
+		let status = apps.status;
+		localStorage.setItem("user-info", JSON.stringify(apps));
+
+		console.log(apps);
+		setApps(apps.data);
+	}
+
+	const showApp = () => {
+		var elements = []
+
+		for (let index in apps) {
+			var app = apps[index];
+			elements.push(<div>
+				<p>Date: {app.date}</p>
+				<p>Time: {app.start_time}</p>
+				<p>End Time: {app.end_time}</p>
+				<p>Appointment Type: {app.appointment_type}</p>
+				<p>Room: {app.room_number}</p>
+				<p>Employee: {app.employee_id}</p>
+				<br />
+			</div>);
 		}
 
+		return elements;
+	}
 
-
-		return (
-			<div>
-				<h1>Welcome {currentUser}</h1>
-				<button onClick={showApp}>Show Appointments</button>
-			</div>
-		)
-
+	return (
+		<div>
+			<h1>Welcome {currentUser}</h1>
+			{loaded ? showApp() : <p>Loading...</p>}
+		</div>
+	)
 }
